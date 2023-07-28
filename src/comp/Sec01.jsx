e2996b8af4b10bfad0fe52b0175db50e5ce68b29
sole <notnowyou@naver.com>
2023-07-28 17:50:58 +0900

    f

--- src/comp/Sec01.jsx
+++ src/comp/Sec01.jsx
@@ -3,44 +3,82 @@ import KOR from '../assets/korea.png'
 import { useState, useRef, useEffect } from 'react';
 function Sec01 () {
     const [tab, setTab] = useState(0);
+    const ulRef = useRef(null);
 
+    // 탭 클릭 시 sel 추가.
     const tapClick = (n) => {
         setTab(prev => n);
     }
+
+    // 드래그 시작 시 첫 위치 기억 변수.
     const [posX, setPosX] = useState(0);
+
+    // 슬라이드 위치 변수.
     const [left, setLeft] = useState(0);
+
+    // 드래그 시작하면
     const dragStartHandler = e => {
+        // 드래그 하면 투명 이미지 없애기
         const img = new Image();
         e.dataTransfer.setDragImage(img, 0, 0);
+
+        // 커서 바꾸기.
         e.dataTransfer.effectAllowed = 'move';
+
+        // 드래그 위치 바꾸기.
         setPosX(prev => e.clientX);
     };
 
     const dragHandler = e => {
+        // 오른쪽, 왼쪽 구분해서 위치 조정.
         if(posX > e.clientX) {
-            console.log('왼?');
             setLeft(prev => prev-1);
         } else {
-            console.log('오?');
             setLeft(prev => prev+1);
         }
     }
     const dragEndHandler = e => {
-        if(left <= -1000) {
+        // 드래그 끝났을 때 맨왼쪽이면 0, 맨오른쪽이면 컨텐츠 끝
+        if(left <= -(ulRef.current.offsetWidth-400)) {
             setLeft(-960);
         } else if(left > 0) {
             setLeft(0);
         }
-        console.log(left);
     }
     const dragOverHandler = e => {
+        // 드래그 오버 다른 이벤트 방지.
         e.preventDefault();
     }
+    const menuClick = (e) => {
+        // 먼저 모든 sel 클래스를 지운 다음.
+        // 클릭한 것만 sel 클래스 추가.
+        const menuAll = document.querySelectorAll('.main-mid > ul > li');
+        menuAll.forEach(el => el.classList.remove('sel'));
+        e.target.classList.add('sel');
+        console.log(e.target.innerText);
+    }
+
+    const slide = (n) => {
+        if(n === 0) {
+            setLeft(prev => prev+50);
+        } else {
+            setLeft(prev => prev-50);
+        }
+    }
+    useEffect(()=>{
+        // 지역/코스에 클릭 이벤트 추가...
+        const menuAll = document.querySelectorAll('.main-mid > ul > li');
+        menuAll.forEach(el => el.addEventListener('click',(e)=>menuClick(e)));
+        return () => {
+            // 클린업으로 클릭 이벤트 삭제...
+            menuAll.forEach(el => el.removeEventListener('click',menuClick));
+        }
+    },[])
     return (
         <section id='sec01'>
             <div className='sec01-top'>
                 <ul>
-                    <li className={tab === 0 ?'sel' : ''} onClick={()=>tapClick(0)}>
+                    <li className={tab === 0 ? 'sel' : ''} onClick={()=>tapClick(0)}>
                         <div className='icon'>
                             <img src={KOR} className='img'/>
                         </div>
@@ -67,8 +105,8 @@ function Sec01 () {
                             <span>서울</span> 인기 여행지
                         </p>
                         <div className='move'>
-                            <i className={posX > 0 ? 'fa-solid fa-circle-chevron-left nomore' : 'fa-solid fa-circle-chevron-left'}></i>
-                            <i className={posX <= 0 ? "fa-solid fa-circle-chevron-right nomore" : "fa-solid fa-circle-chevron-right"}></i>
+                            <i onClick={()=>slide(0)} className={posX > 0 ? 'fa-solid fa-circle-chevron-left nomore' : 'fa-solid fa-circle-chevron-left'}></i>
+                            <i onClick={()=>slide(1)} className={posX < 0 ? "fa-solid fa-circle-chevron-right nomore" : "fa-solid fa-circle-chevron-right"}></i>
                         </div>
                     </div>
                 </div>
@@ -79,6 +117,7 @@ function Sec01 () {
                         onDragEnd={dragEndHandler}
                         onDragOver={dragOverHandler}
                         style={{left:left}}
+                        ref={ulRef}
                     >
                         <li className='sel'>
                             <span>서울</span>
