7c4a667f1cd189477cb9d6af13b741aad56c2c68
sole <notnowyou@naver.com>
2023-08-02 00:53:21 +0900

    f

--- src/comp/Detail01.jsx
+++ src/comp/Detail01.jsx
@@ -1,41 +1,9 @@
-import '../css/detail01.css';
+// import '../css/detail01.css';
 import test from '../assets/csvjson.json';
 import Card from './Detail01/Card';
 const Detail = () => {
     return (
-        <section id='detail01'>
-            {/* <div className="detail-nav">
-                <p>
-                   <span>카테고리</span> 
-                </p>
-                <ul>
-                    <li>
-                        <i className="fa-solid fa-futbol"></i>
-                        <span>활동</span>
-                    </li>
-                    <li className='sel'>
-                        <i className="fa-solid fa-star"></i>
-                        <span>핫플</span>
-                    </li>
-                    <li>
-                        <i className="fa-solid fa-utensils"></i>
-                        <span>음식</span>
-                    </li>
-                    <li>
-                        <i className="fa-solid fa-tree"></i>
-                        <span>자연</span>
-                    </li>
-                    <li>
-                     <i className="fa-solid fa-store"></i>
-                        <span>쇼핑</span>
-                    </li>
-                </ul>
-            </div>
-            <div className="detail-main">
-                <Card />
-            </div> */}
-            <Card />
-        </section>
+        <Card />
     )
 };
 
--- src/comp/Detail01/Card.jsx
+++ src/comp/Detail01/Card.jsx
@@ -2,9 +2,63 @@ import '../../css/card.css'
 
 const Card = () => {
     return (
-        <div>
-            card...
-        </div>
+        <section id="detail01">
+            <div className="detail-left">
+                <ul>
+                    <li>
+                        <p>
+                            <i className="fa-solid fa-baseball-bat-ball"></i>
+                            <span>활동</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <i className="fa-solid fa-tree"></i>
+                            <span>자연</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <i className="fa-solid fa-baseball-bat-ball"></i>
+                            <span>활동</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <i className="fa-solid fa-tree"></i>
+                            <span>자연</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <i className="fa-solid fa-baseball-bat-ball"></i>
+                            <span>활동</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <i className="fa-solid fa-tree"></i>
+                            <span>자연</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <i className="fa-solid fa-baseball-bat-ball"></i>
+                            <span>활동</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <i className="fa-solid fa-tree"></i>
+                            <span>자연</span>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div className="detail-right">
+
+            </div>
+        </section>
     )
 };
 
