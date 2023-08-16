0cb63393301c68f463c2dd55e9b3a6f08e86fb17
sole <notnowyou@naver.com>
2023-08-16 09:29:34 +0900

    ㅠㅠㅠㅠㅠ

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sec01 from "./comp/Sec01";
 import Nav from "./comp/Nav/Nav";
-import Card from "./comp/Detail01/Card";
 import Content from "./comp/Detail02/Content";
 import Mypage from "./comp/Mypage/Mypage";
 import Feed from "./comp/Feed/Feed";
 import Index from "./comp/Index";
-import Login from "./comp/Login/Login";
-import Choice from './comp/Choice'
 import Fav from "./comp/fav/Fav";
 import Search from "./comp/Search/Search";
-import NewLogin from "./comp/Login/NewLogin";
 import Footer from "./comp/Footer/Footer";
-import TTT from "./comp/TTT";
 
 const Router = () => {
     // BrowserRouter는 일반적인 url 방식, HashRouter는 localhost/#/url처럼 #이 붙음.
@@ -22,15 +16,11 @@ const Router = () => {
             <Nav />
             <Routes>
                 <Route path="/" element={<Index />} />
-                <Route path="/list" element={<Card />} />
                 <Route path='/content' element={<Content />} />
                 <Route path='/mypage' element={<Mypage />} />
                 <Route path='/feed' element={<Feed />} />
-                <Route path='/choice' element={<Choice />} />
                 <Route path='/fav' element={<Fav />} />
                 <Route path='/search' element={<Search />} />
-                <Route path='/login' element={<NewLogin />} />
-                <Route path='/ttt' element={<TTT />} />
             </Routes>
             <Footer />
         </BrowserRouter>
